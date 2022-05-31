94eb37872c5aced93b080fa31b710f0c2057ecf3
Alexander Suter <alexander.suter@axonivy.com>
2022-05-31 15:35:48 +0200

    Add job to raise portal version

---
+++ build/raise-portal-version/raise.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+set -e
+
+#
+# Param 1: Version (required)
+#   e.g.: 9.2.0-SNAPSHOT
+# 
+# Param 2: Source Branch (required)
+#   e.g.: master or release/8.0
+#
+# Param 3: dry run (optional)
+#   e.g.: --dry-run
+#
+
+if [ $# -eq 0 ]; then
+  echo "Parameter version required"
+  exit
+fi
+if [ $# -eq 1 ]; then
+  echo "Parameter source branch required"
+  exit
+fi
+
+newVersion=$1
+echo "raise version to ${newVersion}"
+
+sourceBranch=$2
+echo "source branch to checkout repos $2"
+
+dryRun=0
+if [ $# -eq 3 ]; then
+  if [ $3 = "--dry-run" ]; then
+    dryRun=1
+  fi
+fi
+echo "dry run ${dryRun}"
+
+uuid=$(date +%s%N)
+newBranch=raise-portal-version-${newVersion}-${uuid}
+
+# switch to directory of this script
+cd "$(dirname "$0")"
+
+source "../raiseRepo.sh"
+
+function raiseVersionOfOurRepos {
+  repos=(
+    "git@github.com:axonivy/core.git"
+  )
+  runRepoUpdate 'updateSingleRepo' ${repos[@]}
+}
+
+function updateSingleRepo {
+  ./.ivy/raise-portal-version.sh ${newVersion}
+  git commit -a -m "Raise portal version to ${newVersion}"
+}
+
+raiseVersionOfOurRepos
