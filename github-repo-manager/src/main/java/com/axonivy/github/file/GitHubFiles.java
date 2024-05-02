ed79fe2c9fc79b1c7bb7bb079037d09d69cab81f
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-05-02 11:38:36 +0200

    refactor: static constants on interface

--- github-repo-manager/src/main/java/com/axonivy/github/file/GitHubFiles.java
+++ github-repo-manager/src/main/java/com/axonivy/github/file/GitHubFiles.java
@@ -1,13 +1,13 @@
 package com.axonivy.github.file;
 
-public class GitHubFiles {
+public interface GitHubFiles {
 
-  public static final FileMeta LICENSE = new FileMeta("LICENSE", "Add license", "Add_License",
+  FileMeta LICENSE = new FileMeta("LICENSE", "Add license", "Add_License",
       "Add Apache 2.0 license");
-  public static final FileMeta SECURITY = new FileMeta("SECURITY.md", "Add security file", "Add_Security",
+  FileMeta SECURITY = new FileMeta("SECURITY.md", "Add security file", "Add_Security",
       "Add Security.md file to repo");
-  public static final FileMeta CODE_OF_CONDUCT = new FileMeta("CODE_OF_CONDUCT.md", "Add code of conduct file",
-      "Add_Code_of_Conduct", "Add CODE_OF_CONDUCT.md file to repo");
+  FileMeta CODE_OF_CONDUCT = new FileMeta("CODE_OF_CONDUCT.md", "Add code of conduct file", "Add_Code_of_Conduct",
+      "Add CODE_OF_CONDUCT.md file to repo");
 
   public record FileMeta(String filePath, String pullRequestTitle, String branchName, String commitMessage) {
 
