0dcac2ae7c6af58b40667573e8882c81f2650bc5
ivy-edp <elio.dipuma@axonivy.com>
2024-05-03 11:00:58 +0200

    Remove ui-component from raise deps

--- build/raise-deps/raise.sh
+++ build/raise-deps/raise.sh
@@ -48,7 +48,6 @@ function raiseDepsOfOurRepos {
     "git@github.com:axonivy/process-editor-client.git"
     "git@github.com:axonivy/form-editor-client.git"
     "git@github.com:axonivy/config-editor-client.git"
-    "git@github.com:axonivy/ui-components.git"
     "git@github.com:axonivy/theia-ide.git"
   )
   runRepoUpdate 'updateSingleRepo' ${repos[@]}
