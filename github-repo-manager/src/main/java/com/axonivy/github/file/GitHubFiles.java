e6f4050e0ee068233d8265416ae929f8c704cc86
Reguel Wermelinger <reguel.wermelinger@axonivy.com>
2024-06-13 15:51:17 +0200

    MARP-149 use a common code-of-conduct throughout the axonivy-org

--- github-repo-manager/src/main/java/com/axonivy/github/file/GitHubFiles.java
+++ github-repo-manager/src/main/java/com/axonivy/github/file/GitHubFiles.java
@@ -6,8 +6,8 @@ public interface GitHubFiles {
       "Add Apache 2.0 license");
   FileMeta SECURITY = new FileMeta("SECURITY.md", "Add security file", "Add_Security",
       "Add Security.md file to repo");
-  FileMeta CODE_OF_CONDUCT = new FileMeta("CODE_OF_CONDUCT.md", "Remove yet unaligned code of conduct file", "Remove_Code_of_Conduct",
-      "Remove CODE_OF_CONDUCT.md file from repo until aligned");
+  FileMeta CODE_OF_CONDUCT = new FileMeta("CODE_OF_CONDUCT.md", "Add code of conduct file",
+      "Add_Code_of_Conduct", "Add CODE_OF_CONDUCT.md file to repo");
 
   public record FileMeta(String filePath, String pullRequestTitle, String branchName, String commitMessage) {
 
