4c27f88aa2d0cf954045dc2f88fe1ec07a35adb1
Trần Vĩnh Thiện Phúc <phuc.tranvinhthien@axonactive.com>
2024-12-17 14:10:54 +0700

    MARP-756 Handle Feedbacks

--- github-repo-manager/src/main/java/com/axonivy/github/file/GitHubMissingFiles.java
+++ github-repo-manager/src/main/java/com/axonivy/github/file/GitHubMissingFiles.java
@@ -5,13 +5,14 @@ import static com.axonivy.github.file.GitHubFiles.LICENSE;
 import static com.axonivy.github.file.GitHubFiles.SECURITY;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
 import com.axonivy.github.file.GitHubFiles.FileMeta;
 
 public class GitHubMissingFiles {
 
-  private static final List<String> WORKING_ORGANIZATIONS = List.of("axonivy-market");
+  private static final List<String> WORKING_ORGANIZATIONS = List.of("axonivy");
   private static final List<FileMeta> REQUIRED_FILES = List.of(LICENSE, SECURITY, CODE_OF_CONDUCT);
   private static final List<FileMeta> REMOVE_FILES = List.of();
 
@@ -22,17 +23,23 @@ public class GitHubMissingFiles {
       System.out.println("running updates triggered by user "+user);
     }
     int status = 0;
+    List<String> workingOrganizations = getWorkingOrganizations();
     for (var fileMeta : REQUIRED_FILES) {
       var detector = new GitHubMissingFilesDetector(fileMeta, user);
-      var returnedStatus = detector.requireFile(WORKING_ORGANIZATIONS);
+      var returnedStatus = detector.requireFile(workingOrganizations);
       status = returnedStatus != 0 ? returnedStatus : status;
     }
     for (var fileMeta : REMOVE_FILES) {
       var detector = new GitHubFilesRemover(fileMeta, user);
-      var returnedStatus = detector.removeFile(WORKING_ORGANIZATIONS);
+      var returnedStatus = detector.removeFile(workingOrganizations);
       status = returnedStatus != 0 ? returnedStatus : status;
     }
     System.exit(status);
+  }
+
+  private static List<String> getWorkingOrganizations() {
+    String inputtedValue = System.getProperty("GITHUB.WORKING.ORGANIZATIONS", String.join(",", WORKING_ORGANIZATIONS));
+    return Arrays.asList(inputtedValue.split(","));
   }
 
 }
