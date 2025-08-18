00385baa2605b4716139e310f7386db12d5319fc
ttan-axonivy <an.truongthien@axonactive.com>
2025-08-18 10:30:25 +0700

    remove test

--- github-repo-manager/src/test/java/com/axonivy/github/TestGitHubRepoTagger.java
+++ github-repo-manager/src/test/java/com/axonivy/github/TestGitHubRepoTagger.java
@@ -6,9 +6,9 @@ import org.junit.jupiter.api.Test;
 
 class TestGitHubRepoTagger {
 
-  @Test
-  void run() throws IOException {
-    var repo = GitHubProvider.get().getRepository("axonivy/github-repo-manager");
-    new GitHubRepoTagger.Tagger(repo, true, "master", "v9.4.5").run();
-  }
+  // @Test
+  // void run() throws IOException {
+  //   var repo = GitHubProvider.get().getRepository("axonivy/github-repo-manager");
+  //   new GitHubRepoTagger.Tagger(repo, true, "master", "v9.4.5").run();
+  // }
 }
